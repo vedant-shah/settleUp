f07abbded8acce1a0fdd6fff7cb49023cc3d4aa3
Vedant Shah <vedantshah2001@gmail.com>
2023-03-26 12:50:24 +0530

    minor changes

--- src/pages/Split.jsx
+++ src/pages/Split.jsx
@@ -7,10 +7,15 @@ import { BsPiggyBank } from 'react-icons/bs'
 import { FaExchangeAlt } from 'react-icons/fa'
 import { BsPlusCircleFill } from 'react-icons/bs'
 import { RiUserReceived2Line } from 'react-icons/ri'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 function Split() {
     const history = useHistory()
     const { id } = useParams()
     const splitsRef = collection(db, "splits");
+    const [showAddNewExpense, setShowAddNewExpense] = useState(false)
+    const [startDate, setStartDate] = useState(new Date());
     const [split, setSplit] = useState()
     const [currentTab, setCurrentTab] = useState(1)
     const q = query(splitsRef, where("id", "==", id))
@@ -26,8 +31,50 @@ function Split() {
         getCurrentSplit()
     }, [])
 
+    const addNewExpenseModal = <>
+        <div className='' style={{ height: '100vh', width: '100vw', position: 'absolute', zIndex: '100', backgroundColor: '#171717' }}>
+            <div style={{ borderBottom: '1.5px solid #9ec0e5' }} className='d-flex py-4 px-3 align-items-center'>
+                <IoChevronBack onClick={() => { setShowAddNewExpense(false) }} style={{ fontSize: '2.5rem', color: '#9ec0e5' }} />
+                <h5 className='mont mx-4 display-6'>Add New Expense</h5>
+            </div>
+            {split && <form className='mt-4 px-4' >
+                <input type="text" className="form-control" placeholder="Title"
+                // style={{ width: '80%' }}
+                />
+                <div className="input-group mb-3">
+                    <span className="input-group-text" id="basic-addon1">₹</span>
+                    <input type="number" className="form-control" placeholder="Amount"
+                    // style={{ width: '80%' }}
+                    />
+                </div>
+
+                <div className='d-flex align-items-center'>
+                    <span>Date:</span>
+                    <DatePicker dateFormat={"dd/MM/yyyy"} selected={startDate} onChange={(date) => setStartDate(date)} />
+                </div>
+
+                <div className='d-flex align-items-center '>
+                    <span>Paid By:</span>
+                    <select
+                        style={{ width: "50%" }}
+                        className="form-select mx-3"
+                        // {...register("currency")}
+                        aria-label="Default select example"
+                    >
+                        {split.participants.map(friend => {
+                            return (<option key={Math.random()} value={friend}>{friend}</option>)
+                        })}
+
+                    </select>
+                </div>
+            </form>}
+        </div>
+    </>
+    let { name } = JSON.parse(localStorage.getItem("user"));
+    name = name.substring(0, name.indexOf(' ')).toLowerCase()
     return (<>
         {split && <div style={{ height: '100vh', width: '100vw' }}>
+            {showAddNewExpense && addNewExpenseModal}
             <div
                 className="d-flex justify-content-center flex-column p-0 align-items-start"
                 style={{
@@ -37,9 +84,14 @@ function Split() {
                     // borderRadius: '0 0 20px 20px',
                     backgroundColor: "#141414",
                 }}>
-                <div className='d-flex p-2 align-items-center'>
-                    <IoChevronBack onClick={() => { history.push('/home') }} style={{ fontSize: '2.5rem', color: '#9ec0e5' }} />
-                    <h2 className='mont mx-4 display-4'>{split.title}</h2>
+                <div className='d-flex w-100 px-4 align-items-center justify-content-between'>
+                    <div className='d-flex py-3 align-items-center'>
+                        <IoChevronBack onClick={() => { history.push('/home') }} style={{ fontSize: '2.5rem', color: '#9ec0e5' }} />
+                        <h2 className='mont mx-4 display-4'>{split.title}</h2>
+                    </div>
+                    <div className='px-3' style={{ backgroundColor: split.balances[name] < 0 ? '#edbe90' : '#89e289', color: '#141414', borderRadius: '10px' }}>
+                        ₹ {split.balances[name]}
+                    </div>
                 </div>
                 <div className='mt-3 w-100 d-flex justify-content-around'>
                     <div style={{ borderBottom: currentTab === 1 ? '2px solid #cb2727' : undefined }} onClick={() => { setCurrentTab(1) }}>
@@ -57,7 +109,7 @@ function Split() {
 
                 </div>
             </div>
-            <BsPlusCircleFill className='button' onClick={() => { history.push("/addsplit") }} />
+            {currentTab === 1 && <BsPlusCircleFill className='button' onClick={() => { setShowAddNewExpense(true) }} />}
         </div>}
     </>)
 }
